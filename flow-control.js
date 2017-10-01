f98f8f03ff11aa7b9ddd1ebda882fa31e81e15f7
Chris Nunez <cenunez47@gmail.com>
2017-10-01 20:53:42 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,10 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  case age > 12 && age < 20:
-    return "You are a teenager"
-  default:
-    return "You have an age"
+  switch (age) {
+    case age > 12 && age < 20:
+      return "You are a teenager"
+    default:
+      return "You have an age"
+    }
 }
